70a83d8ab3f4fad382709393c4592b3a83db032b
jahwsuf <jahwsuf@gmail.com>
2020-07-28 19:47:32 +0700

    mid-prestige tests

--- Testing/cases/calculations/beforeAccelBoostWithCrystalUpgrade.js
+++ Testing/cases/calculations/beforeAccelBoostWithCrystalUpgrade.js
@@ -9,14 +9,14 @@ function setSaveFromFixture(name) {
     // Prevent offline loading.  (It's simpler; no need to wait for the calculator.)
     // Write an alternate version when targetting offline, as controlling _how much_ offline
     // will require .offlineTick manipulation.
-    saveToLoad.offlinetick = Date.now();
+    saveToLoad.offlinetick = Date.now() + 1000;
     localStorage.setItem("Synergysave2", btoa(JSON.stringify(saveToLoad)));
 }
 
 // This test-specification format is due to use of Mocha.  https://mochajs.org/#getting-started
 // describe - creates a named grouping of tests.  May be nested.
 // it - a single unit test.
-describe("Calculations:  AtFirstPrestige", function() {
+describe("Calculations:  AtBoostWithCrystalUpgrade", function() {
     // Runs this code once for the entire test grouping BEFORE any tests run
     before(function(done) {
         this.timeout(3000); // 6 seconds.
@@ -30,7 +30,7 @@ describe("Calculations:  AtFirstPrestige", function() {
         fixture.setBase('');
 
         // Set the game to a clean state, no offline ticks.  They get in the way of test init.
-        setSaveFromFixture("TestSaves/AtFirstPrestige.json");
+        setSaveFromFixture("TestSaves/AtBoostWithCrystalUpgrade.json");
 
         // Loads the actual test page.
         fixture.load("/index.html");
@@ -50,14 +50,15 @@ describe("Calculations:  AtFirstPrestige", function() {
 
     it("correctly calculates total coin buildings earned", function() {
         calculateTotalCoinOwned(); // value saved to window.totalCoinOwned
-        assert.equal(totalCoinOwned, 90 + 30 + 20 + 18 + 12);
+        // hmm... seems to vary at load time.  :(
+        assert.equal(totalCoinOwned, 935 + 461 + 303 + 224 + 177);
     })
 
     it("correctly calculates total accelerator boosts", function() {
         // Extreme early-game - no freebies.
         calculateTotalAcceleratorBoost();
-        assert.closeTo(freeAcceleratorBoost, 0, 0.001);
-        assert.closeTo(totalAcceleratorBoost, 0, 0.001);
+        assert.closeTo(freeAcceleratorBoost, 2, 0.001);
+        assert.closeTo(totalAcceleratorBoost, 3, 0.001);
     });
 
     it("correctly calculates the rune recycle EXP modifier", function() {
@@ -71,12 +72,13 @@ describe("Calculations:  AtFirstPrestige", function() {
         }
     });
 
-    // No runes are accessible before prestige.
-    // it("correctly calculates rune EXP needed to level", function() {
-    //     for(let r=1; r <= 5; r++) {
-
-    //     }
-    // });
+    it("correctly calculates total rune EXP needed to level", function() {
+        // Does not care about current rune EXP.
+        let expectedRuneEXP = [383.328, 0.832, 1.872, 3.328, 208];
+        for(let r=0; r < 5; r++) {
+            assert.closeTo(calculateRuneExpToLevel(r), expectedRuneEXP[r], 0.001);
+        }
+    });
 
     it("correctly calculates rune caps", function() {
         // Caps cannot be raised before prestige.
@@ -87,7 +89,7 @@ describe("Calculations:  AtFirstPrestige", function() {
 
     it("correctly calculates offerings at prestige", function() {
         // 1 - offerings upon prestige.
-        assert.closeTo(calculateOfferings(1), 14, 0.001);
+        assert.closeTo(calculateOfferings(1), 0, 0.001);
     });
 
     // Obtainium not yet unlocked.
@@ -113,9 +115,10 @@ describe("Calculations:  AtFirstPrestige", function() {
 
     it("correctly calculates rune levels", function() {
         calculateRuneLevels();
-        // No runes yet, so they default to level one effects.
-        for(let i=1; i <= 5; i++) {
-            assert.closeTo(window["rune" + i + "level"], 1, 0.001);
+
+        let expectedRuneLevels = [11, 1, 1, 1, 1]
+        for(let i=0; i < 5; i++) {
+            assert.closeTo(player.runelevels[i], expectedRuneLevels[i], 0.001);
         }
     });
 
@@ -161,38 +164,38 @@ describe("Calculations:  AtFirstPrestige", function() {
 
     it("correctly calculates accelerators, boosts, and their effects [updateAllTick()]", function() {
         updateAllTick();
-        assert.equal(freeAccelerator, 0, "Unexpected free accelerator count");
-        assert.equal(totalAccelerator, 18, "Unexpected total accelerator count");
-        assert.equal(freeAcceleratorBoost, 0, "Unexpected free accelerator boost count");
-        assert.equal(totalAcceleratorBoost, 0, "Unexpected total accelerator boost count");
-        assert.isTrue(acceleratorEffect.equals_tolerance(new Decimal("5.5599731349224e0"), 0.001), "Expected " + acceleratorEffect + " to equal " + (new Decimal("5.5599731349224e0")));
+        assert.equal(freeAccelerator, 140, "Unexpected free accelerator count");
+        assert.equal(totalAccelerator, 274, "Unexpected total accelerator count");
+        assert.equal(freeAcceleratorBoost, 2, "Unexpected free accelerator boost count");
+        assert.equal(totalAcceleratorBoost, 3, "Unexpected total accelerator boost count");
+        assert.isTrue(acceleratorEffect.equals_tolerance(new Decimal("2.14133719e15"), 0.001), "Unexpected total accelerator effect");
     });
 
     it("correctly calculates multipliers and their effects [updateAllMultipliers()]", function() {
         updateAllMultiplier();
-        assert.equal(freeMultiplier, 0, "Unexpected free multiplier count");
-        assert.equal(totalMultiplier, 8, "Unexpected total multiplier count");
+        assert.equal(freeMultiplier, 37, "Unexpected free multiplier count");
+        assert.equal(totalMultiplier, 117, "Unexpected total multiplier count");
         assert.equal(freeMultiplierBoost, 0, "Unexpected free multiplier boost count");
         assert.equal(totalMultiplierBoost, 0, "Unexpected total multiplier boost count");
-        assert.isTrue(multiplierEffect.equals_tolerance(new Decimal("2.56e2"), 0.001), "Expected " + multiplierEffect + " to equal " + (new Decimal("2.56e2")));
+        assert.isTrue(multiplierEffect.equals_tolerance(new Decimal("1.66153499e35"), 0.001), "Unexpected total multiplier effect");
     });
 
     it("correctly calculates miscellaneous multipliers [multipliers()])", function() {
         multipliers();
-        assert.isTrue(prestigeMultiplier.equals_tolerance(new Decimal("1e0"), 0.001), "Unexpected prestige multiplier");
+        assert.isTrue(prestigeMultiplier.equals_tolerance(new Decimal("1.23359935502697e3"), 0.001), "Unexpected prestige multiplier");
         assert.closeTo(buildingPower, 1, 0.001, "Unexpected building power value");
         assert.isTrue(reincarnationMultiplier.equals_tolerance(new Decimal("1e0"), 0.001), "Unexpected reincarnation multiplier value");
         assert.isTrue(antMultiplier.equals_tolerance(new Decimal("1e0"), 0.001), "Unexpected ant multiplier value");
 
-        assert.isTrue(globalCoinMultiplier.equals_tolerance(new Decimal("1.42333883225e3"), 0.001), "Unexpected global coin multiplier value");
+        assert.isTrue(globalCoinMultiplier.equals_tolerance(new Decimal("1.9030372116756216e64"), 0.001), "Unexpected global coin multiplier value");
 
-        assert.isTrue(coinOneMulti.equals_tolerance(new Decimal("6.626536111e2"), 0.001), "Unexpected 'coin one' multiplier value");
-        assert.isTrue(coinTwoMulti.equals_tolerance(new Decimal("6.626536111e2"), 0.001), "Unexpected 'coin two' multiplier value");
-        assert.isTrue(coinThreeMulti.equals_tolerance(new Decimal("6.626536111e2"), 0.001), "Unexpected 'coin three' multiplier value");
-        assert.isTrue(coinFourMulti.equals_tolerance(new Decimal("6.626536111e2"), 0.001), "Unexpected 'coin four' multiplier value");
-        assert.isTrue(coinFiveMulti.equals_tolerance(new Decimal("6.626536111e2"), 0.001), "Unexpected 'coin five' multiplier value");
+        assert.isTrue(coinOneMulti.equals_tolerance(new Decimal("6.937431092693947e19"), 0.001), "Unexpected 'coin one' multiplier value");
+        assert.isTrue(coinTwoMulti.equals_tolerance(new Decimal("3.886364070049174e10"), 0.001), "Unexpected 'coin two' multiplier value");
+        assert.isTrue(coinThreeMulti.equals_tolerance(new Decimal("3.886364070049174e10"), 0.001), "Unexpected 'coin three' multiplier value");
+        assert.isTrue(coinFourMulti.equals_tolerance(new Decimal("3.886364070049174e10"), 0.001), "Unexpected 'coin four' multiplier value");
+        assert.isTrue(coinFiveMulti.equals_tolerance(new Decimal("3.886364070049174e10"), 0.001), "Unexpected 'coin five' multiplier value");
 
-        assert.isTrue(globalCrystalMultiplier.equals_tolerance(new Decimal("1e0"), 0.001), "Unexpected global crystal multiplier");
+        assert.isTrue(globalCrystalMultiplier.equals_tolerance(new Decimal("5.166499e2"), 0.001), "Unexpected global crystal multiplier");
         assert.isTrue(globalMythosMultiplier.equals_tolerance(new Decimal("1e0"), 0.001), "Unexpected global mythos multiplier");
         assert.isTrue(grandmasterMultiplier.equals_tolerance(new Decimal("1e0"), 0.001), "Unexpected grandmaster multiplier");
         assert.isTrue(globalAntMult.equals_tolerance(new Decimal("1.00069e0"), 0.001), "Unexpected global ant (speed) multiplier");
@@ -204,7 +207,7 @@ describe("Calculations:  AtFirstPrestige", function() {
 
     it("correctly calculates tax effects", function() {
         calculatetax();
-        assert.isTrue(taxdivisor.equals_tolerance(new Decimal("1.001804e0"), 0.001), "Unexpected tax divisor");
+        assert.isTrue(taxdivisor.equals_tolerance(new Decimal("1.1648365e0"), 0.001), "Unexpected tax divisor");
         assert.isTrue(taxdivisorcheck.equals_tolerance(new Decimal("4.0324392229e31930"), 0.001), "Unexpected tax cap");
     });
 
